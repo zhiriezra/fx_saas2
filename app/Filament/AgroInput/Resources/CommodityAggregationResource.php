4310252464a769e5952d4bfe5b9f5dec52bcbc84
zhiriezra <zhiriezra@gmail.com>
2025-05-20 23:40:09 +0100

    fixed navigation badge

--- app/Filament/AgroInput/Resources/CommodityAggregationResource.php
+++ app/Filament/AgroInput/Resources/CommodityAggregationResource.php
@@ -12,6 +12,7 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Facades\Filament;
 
 class CommodityAggregationResource extends Resource
 {
--- app/Filament/AgroInput/Resources/CommodityPricingReportResource.php
+++ app/Filament/AgroInput/Resources/CommodityPricingReportResource.php
@@ -15,6 +15,7 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Illuminate\Support\Collection;
+use Filament\Facades\Filament;
 
 class CommodityPricingReportResource extends Resource
 {
