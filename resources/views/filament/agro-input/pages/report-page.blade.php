af0848bee52f5d892e65945b3ea78c8d401dfd55
zhiriezra <zhiriezra@gmail.com>
2025-05-21 13:51:10 +0100

    Mafa M&E

--- resources/views/filament/agro-input/pages/report-page.blade.php
+++ resources/views/filament/agro-input/pages/report-page.blade.php
@@ -1,9 +1,17 @@
 <x-filament-panels::page>
     <div class="w-full h-screen">
-        <iframe
+        <!-- <iframe
             title="FMAN Report"
             class="w-full h-full border-none"
             src="https://app.powerbi.com/view?r=eyJrIjoiMDE3ZGRmZDctZTJmMS00MTNiLThkMjgtMDc1MTY2NDk1YzZmIiwidCI6IjY0ODMzYzU4LWFjYzQtNDFlMy1iZDk2LWYxZWUxOWE3NzZmMSJ9&pageName=c86f1232309db1bbce9d" frameborder="0"
+            allowFullScreen="true">
+        </iframe> -->
+
+        <iframe 
+            title="Mafa Analysis Report" 
+            class="w-full h-full border-none"
+            src="https://app.powerbi.com/view?r=eyJrIjoiMzYzNGZlMDQtZWU0Ni00OTU4LTkyNTItODhkNTZkMDlmYTM1IiwidCI6IjU4ODBmZDMzLTYyZmEtNGM0Ny1iYTFmLWJhMWE1ODM5Mjc2NyJ9" 
+            frameborder="0" 
             allowFullScreen="true">
         </iframe>
     </div>
