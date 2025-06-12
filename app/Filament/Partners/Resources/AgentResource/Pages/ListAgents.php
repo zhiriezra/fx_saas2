ed1102b3ed3340844a9ea102cf119ad007744a3e
zhiriezra <zhiriezra@gmail.com>
2025-06-12 15:39:33 +0100

    partners

--- app/Filament/Partners/Resources/AgentResource/Pages/ListAgents.php
+++ app/Filament/Partners/Resources/AgentResource/Pages/ListAgents.php
@@ -5,6 +5,8 @@ namespace App\Filament\Partners\Resources\AgentResource\Pages;
 use App\Filament\Partners\Resources\AgentResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListAgents extends ListRecords
 {
@@ -15,5 +17,10 @@ class ListAgents extends ListRecords
         return [
             Actions\CreateAction::make(),
         ];
+    }
+
+    protected function paginateTableQuery(Builder $query): Paginator
+    {
+        return $query->fastPaginate(($this->getTableRecordsPerPage() === 'all') ? $query->count() : $this->getTableRecordsPerPage());
     }
 }
